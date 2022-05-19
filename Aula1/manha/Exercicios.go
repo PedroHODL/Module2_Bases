a0cf5db35678a3c7b8ac2c8f02d1008aceca291f
PedroHODL <pedrohenrique.lopes@mercadolivre.com>
2022-05-19 17:09:21 -0300

    Develop (#4)
    * renomear arquivos go
    
    * mudança no formato
    
    * faltou texto

--- Aula1/manha/Exercicios.go
+++ Aula1/manha/Exercicios.go
@@ -21,3 +21,53 @@ func Ex_2() {
 		"\na umidade do ar é de ", umidade, "%",
 		"\ne a pressão é de ", pressao, "mbar")
 }
+
+//Ex-3 - Declaração de variáveis
+/**
+Um professor de programação está corrigindo as avaliações de seus alunos na disciplina de
+Programação I para fornecer-lhes o feedback correspondente. Um dos pontos do exame é
+declarar diferentes variáveis.
+
+Ajude o professor com as seguintes questões:
+	1. Verifique quais dessas variáveis declaradas pelo aluno estão corretas.
+	2. Corrigir as incorrectas.
+
+		var 1nome string
+		var sobrenome string
+		var int idade
+		1sobrenome := 6
+		var licenca_para_dirigir = true
+		var estatura da pessoa int
+		quantidadeDeFilhos := 2
+
+	Corrigido:
+		var nome1 string
+		var sobrenome string
+		var idade int
+		sobrenome1 := 6
+		var licenca_para_dirigir bool = true
+		var estatura_da_pessoa int
+		quantidadeDeFilhos := 2
+**/
+
+//Ex-4 - Tipos de dados
+/**
+Um estudante de programação tentou fazer declarações de variáveis com seus respectivos
+tipos em Go mas teve várias dúvidas ao fazê-lo. A partir disso, ele nos deu seu código e
+pediu a ajuda de um desenvolvedor experiente que pode:
+
+	● Revisar o código e realizar as devidas correções.
+
+		var sobrenome string = "Silva"
+		var idade int = "25"
+		boolean := "false";
+		var salario string = 4585.90
+		var nome string = "Fellipe"
+
+	Corrigido:
+		var sobrenome string = "Silva"
+		var idade int = 25
+		boolean := false
+		var salario float32 = 4585.90
+		var nome string = "Fellipe"
+**/
